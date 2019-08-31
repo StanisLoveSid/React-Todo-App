8cbe1a35282c20b4193ca5b24eea48dc251aac6f
StanisLove Sid <joingamings@gmail.com>
2019-08-31 22:59:26 +0300

    add done effect

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,24 +3,35 @@ import React, { Component } from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
-  constructor() {
-    super();
 
-    this.onLabelClick = () => {
-      console.log(this.props.label)
-    }
+  state = {
+    done: false
   }
+
+  onLabelClick = () => {
+    this.setState({
+      done: true
+    });
+  };
+  
 
   render() {
     const { label, important = false } = this.props;
+    const { done } = this.state;
 
     const style = {
       color: important ? 'steelblue' : 'black',
       fontWeight: important ? 'bold' : 'normal'
     };
+
+    let classNames = 'todo-list-item';
+
+    if(done) {
+      classNames += ' done';
+    }
   
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
           className="todo-list-item-label"
           onClick={ this.onLabelClick }
