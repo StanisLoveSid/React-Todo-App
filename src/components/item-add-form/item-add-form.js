b5a9d63404b832d6ee892f1280b653874dcec0b8
StanisLove Sid <joingamings@gmail.com>
2019-09-02 23:46:12 +0300

    add fullfunctional items

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,16 +3,38 @@ import React, { Component } from 'react';
 import './item-add-form.css'
 
 export default class ItemAddForm extends Component {
-  render() {
-    const { onAdded } = this.props;
 
+  state = {
+    label: ''
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAdded(this.state.label)
+    this.setState({
+      label: ''
+    });
+  }
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  }
+
+  render() {
     return(
-      <div className='item-add-form'>
-        <button className='btn btn-outline-secondary'
-                onClick={() => onAdded('lol')}>
+      <form className='item-add-form d-flex'
+            onSubmit={this.onSubmit}>
+        <input type='text'
+               className='form-control'
+               onChange={this.onLabelChange}
+               placeholder='What needs to be done'
+               value={this.state.label}/>
+        <button className='btn btn-outline-secondary'>
           Add Item
         </button>
-      </div>
+      </form>
     )
   }
 }
